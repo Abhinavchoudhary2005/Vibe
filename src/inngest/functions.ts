2ca1908ac074799a56c7c2995b3c27eb3116a7a6
Abhinav Choudhary <cabhinav2005@gmail.com>
2025-08-23 23:47:36 +0530

    setup first agent tool

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,7 +1,8 @@
 import { inngest } from "./client";
-import { gemini, createAgent } from "@inngest/agent-kit";
+import { gemini, createAgent, createTool } from "@inngest/agent-kit";
 import { Sandbox } from "@e2b/code-interpreter";
 import { getSandbox } from "./utils";
+import z from "zod";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -17,6 +18,38 @@ export const helloWorld = inngest.createFunction(
       system:
         "You are an expert text summarizer. Given a piece of text, your job is to provide a concise and accurate summary, capturing the main points and key information.",
       model: gemini({ model: "gemini-1.5-flash" }),
+      tools: [
+        createTool({
+          name: "terminal",
+          description: "use the terminal to run the command",
+          parameters: z.object({
+            command: z.string(),
+          }),
+
+          handler: async ({ command }, { step }) => {
+            return await step?.run("terminal", async () => {
+              const buffers = { stdout: "", stderr: "" };
+              try {
+                const sandbox = await getSandbox(sandboxId);
+                const result = sandbox.commands.run(command, {
+                  onStdout: (data: string) => {
+                    buffers.stdout += data;
+                  },
+                  onStderr: (data: string) => {
+                    buffers.stderr += data;
+                  },
+                });
+                return (await result).stdout;
+              } catch (e) {
+                console.error(
+                  `command failed ${e} \nstdout: ${buffers.stdout} \nstderr: ${buffers.stderr}`
+                );
+                return `command failed ${e} \nstdout: ${buffers.stdout} \nstderr: ${buffers.stderr}`;
+              }
+            });
+          },
+        }),
+      ],
     });
 
     const { output } = await codeWriterAgent.run(`${event.data.value}`);
